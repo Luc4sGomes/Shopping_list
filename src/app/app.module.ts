32be08041e894b968fa6c875beef1aa05e760bb8
lucas <lucasgominhaa448@gmail.com>
2022-04-04 10:51:39 -0300

    added initial part that contains the initial code of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListShoppingComponent } from './list-shopping/list-shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListShoppingComponent
   ],
   imports: [
     BrowserModule
